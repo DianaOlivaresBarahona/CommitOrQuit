951966be3c8dff6280dc3fd5103960bf2aa3408f
Rebecca Molund Labeeb <rebecca.labeeb@gmail.com>
2025-02-27 12:13:14 +0100

    Fixed Navbar and header. Added margin to header. Fixed Navbar and header. Added margin to header. Hopefully this solved the problem,

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,31 +4,29 @@ import SearchField from "./searchfield";
 import { useEffect } from "react";
 
 const Navbar = () => {
-    useEffect(() => {
-        if (window.lucide) {
-            window.lucide.createIcons();
-        }
-    }, []);
+  useEffect(() => {
+    if (window.lucide) {
+      window.lucide.createIcons();
+    }
+  }, []);
 
-    return (
-        <header>
-            <nav className="Navbar">  
-                <button className="menu-button">
-                    <i data-lucide="menu"></i>
-                </button>
+  return (
+    <nav className="Navbar">
+      <button className="menu-button">
+        <i data-lucide="menu"></i>
+      </button>
 
-                <div className="logo">
-                    <h1 className="logo-text">KALEIDO</h1> 
-                </div>
-                
-                    <SearchField />
+      <div className="logo">
+        <h1 className="logo-text">KALEIDO</h1>
+      </div>
 
-                    <button className="save-button">
-                        <i data-lucide="heart"></i>
-                    </button>
-            </nav>
-        </header>
-    );
+      <SearchField />
+
+      <button className="save-button">
+        <i data-lucide="heart"></i>
+      </button>
+    </nav>
+  );
 };
 
 export default Navbar;
