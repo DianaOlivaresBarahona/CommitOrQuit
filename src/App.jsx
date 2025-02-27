553b3a0885327aa252651d6f0759e6d8920f645c
Batoul Ali <batoulali@MacBook-Air-som-tillhor-Batoul.local>
2025-02-27 12:31:47 +0100

    Lade till autentisering och profilsida med React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import "./stylesheet/navbar.css";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
-
+import Auth from "./components/Auth";  
+import Profile from "./pages/Profile"; 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Auth />} />  
+        <Route path="/profile" element={<Profile />} />  
       </Routes>
       <Footer />
-    </>
+    </Router>
   );
 };
 
