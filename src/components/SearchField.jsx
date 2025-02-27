b10accfb1f4deff619de84a6562944a201d5346a
Rebecca Molund Labeeb <rebecca.labeeb@gmail.com>
2025-02-27 13:14:52 +0100

    Still fixing...

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -2,6 +2,7 @@ import Input from "./Input";
 import React, { useState } from "react";
 import "../stylesheet/searchField.css";
 import ButtonText from "./ButtonText";
+/* import useProducts from "../context/UseProducts"; */
 
 const SearchField = () => {
   const [searchItem, setSearchItem] = useState("");
