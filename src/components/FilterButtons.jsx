1ee5f02427098787c1c3111d59bea1dc26bb36ff
Aleksandra Shabardina <sasha.sha34@gmail.com>
2025-02-27 17:44:20 +0100

    Changed apparel to women's and men's clothing

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -14,8 +14,8 @@ const FilterButtons = () => {
             <button className="all-button">All</button>
             <button className="jewelry-button">Jewelry</button>
             <button className="electronics-button">Electronics</button>
-            <button className="men-apparel-button">Men's Apparel</button>
-            <button className="women-apparel-button">Women's Apparel</button>
+            <button className="mens-clothing-button">Men's Clothing</button>
+            <button className="womens-clothing-button">Women's Clothing</button>
             <button className="filter-button">
                 <i data-lucide="filter"></i>
             </button>
