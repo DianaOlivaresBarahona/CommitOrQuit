2c1534a1b8b042b4cb23534a884d74cf65f56704
Emma Druzic <emmadruzic@gmail.com>
2025-02-28 15:40:42 +0100

    fixed css for order confirmation

--- src/pages/ConfirmationPage.jsx
+++ src/pages/ConfirmationPage.jsx
@@ -1,20 +1,50 @@
+// import React from "react";
+// import "../stylesheet/ConfirmationPage.css";
+// import { BsBagCheck } from "react-icons/bs";
+
+
+
+// const ConfirmationPage = () => {
+//     return (
+//         <div className="confirmation-page">
+//             <main className="content">
+//                 <h2>Order confirmation</h2>
+//                 <span className="icon">
+//                     <BsBagCheck />
+//                 </span>
+//                 <div className="thank-you">
+//                 <h3>Thank you!</h3>
+//                 <p>A confirmation has been sen to your email.<br/>Since you're here, join our list for discounts!</p>
+//                 </div>
+//                 <div className="subscribe">
+//                     <input type="email" placeholder="Email Address" />
+//                     <button className="subscribe-btn">Yes, sign me up!</button>
+//                 </div>
+//             </main>
+//         </div>
+//     );
+// };
+
+
+// export default ConfirmationPage;
+
 import React from "react";
 import "../stylesheet/ConfirmationPage.css";
 import { BsBagCheck } from "react-icons/bs";
 
-
-
 const ConfirmationPage = () => {
     return (
         <div className="confirmation-page">
+           
             <main className="content">
-                <h2>Order confirmation</h2>
-                <span className="icon">
-                    <BsBagCheck />
-                </span>
                 <div className="thank-you">
-                <h3>Thank you!</h3>
-                <p>A confirmation has been sen to your email.<br/>Since you're here, join our list for discounts!</p>
+                    <h2>Order confirmation</h2>
+                    <span className="icon">
+                        <BsBagCheck />
+                    </span>
+                    <h3>Thank you!</h3>
+                    <p>A confirmation has been sent to your email.<br />
+                        Since you're here, join our list for discounts!</p>
                 </div>
                 <div className="subscribe">
                     <input type="email" placeholder="Email Address" />
@@ -24,6 +54,5 @@ const ConfirmationPage = () => {
         </div>
     );
 };
-
 
 export default ConfirmationPage;
