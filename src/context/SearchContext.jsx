4c010a91f8b72732438a9a26611de752b81080b0
Aleksandra Shabardina <sasha.sha34@gmail.com>
2025-02-28 11:38:05 +0100

    Added filter function to the filter buttons

--- src/context/SearchContext.jsx
+++ src/context/SearchContext.jsx
@@ -5,18 +5,26 @@ const SearchContext = createContext();
 
 export const SearchProvider = ({ children }) => {
   const [searchItem, setSearchItem] = useState(""); // Global sökterm
+  const [category, setCategory] = useState("All"); // (+) Ny kategori-state
   const { data, error, isLoading } = useProducts(); // Hämtar produkter
 
   // 🔥 Flyttad filtrering hit
   const filteredProducts = data
-    ? data.filter((product) =>
-        product.title.toLowerCase().includes(searchItem.toLowerCase())
-      )
-    : [];
+    ? data.filter((product) => {
+      const matchesSearch = product.title
+        .toLowerCase()
+        .includes(searchItem.toLowerCase());
+
+        const matchesCategory =
+          category === "All" || product.category === category.toLowerCase();
+        
+          return matchesSearch && matchesCategory;
+        })
+      : [];
 
   return (
     <SearchContext.Provider
-      value={{ searchItem, setSearchItem, filteredProducts, error, isLoading }}
+      value={{ searchItem, setSearchItem, category, setCategory, filteredProducts, error, isLoading }}
     >
       {children}
     </SearchContext.Provider>
