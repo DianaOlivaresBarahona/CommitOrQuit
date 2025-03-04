de97bdc90d4df42bff27ed67073059b32075c201
Aleksandra Shabardina <sasha.sha34@gmail.com>
2025-03-04 12:56:50 +0100

    Added feedback to Add to cart button when clicked

--- src/components/AddCartBtn.jsx
+++ src/components/AddCartBtn.jsx
@@ -1,9 +1,18 @@
+import React, { useState } from "react";
 import "../stylesheet/addCartBtn.css";
 
 const AddCartBtn = ({ onClick }) => {
+  const [added, setAdded] = useState(false);
+
+  const handleClick = () => {
+    onClick();
+    setAdded(true);
+    setTimeout(() => setAdded(false), 1500);
+  };
+
   return (
-    <button className="add-to-cart-button" onClick={onClick}>
-      Add to Cart
+    <button className={`add-to-cart-button ${added ? "added" : ""}`} onClick={handleClick}>
+      {added ? "Added!" : "Add to Cart"}
     </button>
   );
 };
