66f760cd34fe2c8c6544984135bb329bb191a830
DianaOlivaresBarahona <olivaresbarahona@live.se>
2025-03-05 11:26:47 +0100

    fixa css namn

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useSearch } from "../context/SearchContext";
 import { useCart } from "../context/CartContext";
-import "../stylesheet/productDetail.css";
+import "../stylesheet/ProductDetail.css";
 
 import { Heart } from "lucide-react";
 import { useEffect } from "react";
